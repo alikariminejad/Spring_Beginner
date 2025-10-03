523fec52de8e54ded174a0137b4b11cb0e110cd7
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2025-10-03 23:12:09 +0330

    Created More Java Spring Beans in Spring Java Configuration File

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App02HelloWorldSpring.java
@@ -2,6 +2,7 @@ package com.in28minutes.learn_spring_framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class App02HelloWorldSpring {
 
 	public static void main(String[] args) {
@@ -13,6 +14,10 @@ public class App02HelloWorldSpring {
 		
 		//3: Retrieving Beans managed by Spring
 		System.out.println(context.getBean("name"));
+		System.out.println(context.getBean("age"));
+		System.out.println(context.getBean("person"));
+		System.out.println(context.getBean("address"));
+		
 	}
 
 }
--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/HelloWorldConfiguration.java
@@ -3,6 +3,10 @@ package com.in28minutes.learn_spring_framework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+record Person(String name, int age) {};
+record Address(String firstLine, String city) {};
+
+
 @Configuration
 public class HelloWorldConfiguration {
 
@@ -10,5 +14,20 @@ public class HelloWorldConfiguration {
 	@Bean
 	public String name() {
 		return "Ali";
+	}
+	
+	@Bean
+	public int age() {
+		return 27;
+	}
+	
+	@Bean
+	public Person person() {
+		return new Person("Jack", 23);
+	}
+	
+	@Bean
+	public Address address() {
+		return new Address("unknown street", "Germany");
 	}
 }

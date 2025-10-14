5938dceb7bf6c0b4dd68fbb5f00c448328fbabbf
alikariminejad <kariminejad.ali.ak@gmail.com>
2025-10-14 09:07:34 +0330

    commit

--- learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learn_spring_framework/App01GamingBasicJava.java
@@ -8,7 +8,7 @@ import com.in28minutes.learn_spring_framework.game.PacmanGame;
 public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
-		
+		// comment
 		//var game = new MarioGame();
 		//var game = new SuperContraGame();
 		var game = new PacmanGame();

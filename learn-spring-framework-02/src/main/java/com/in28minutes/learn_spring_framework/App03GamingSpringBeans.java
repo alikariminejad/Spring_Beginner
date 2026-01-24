253b0123326827ecebe3a5b04f573e379b5292ac
alikariminejad <kariminejad.ali.ak@gmail.com>
2026-01-24 20:45:24 +0330

    Create beans using @Component

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/App03GamingSpringBeans.java
@@ -2,6 +2,7 @@ package com.in28minutes.learn_spring_framework;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learn_spring_framework.game.GameRunner;
@@ -11,14 +12,8 @@ import com.in28minutes.learn_spring_framework.game.PacmanGame;
 
 
 @Configuration
+@ComponentScan("package com.in28minutes.learn_spring_framework.game")
 public class App03GamingSpringBeans {
-
-	
-	@Bean
-	public GamingConsole game() {
-		var game = new PacmanGame();
-		return game;
-	}
 	
 	@Bean
 	public GameRunner gameRunner(GamingConsole game) {

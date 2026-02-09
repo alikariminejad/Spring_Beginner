1e9acc8bf7f18da3617de463b07a23d9da4a5646
alikariminejad <kariminejad.ali.ak@gmail.com>
2026-02-09 23:50:26 +0330

    dependency injection

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/GamingAppLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/game/GamingAppLauncherApplication.java
@@ -1,22 +1,20 @@
-package com.in28minutes.learn_spring_framework;
+package com.in28minutes.learn_spring_framework.game;
+
+import java.util.Arrays;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-import com.in28minutes.learn_spring_framework.game.GameRunner;
-import com.in28minutes.learn_spring_framework.game.GamingConsole;
-
 
 
 @Configuration
-@ComponentScan("package com.in28minutes.learn_spring_framework.game")
+@ComponentScan
 public class GamingAppLauncherApplication {
 	
 	public static void main(String[] args) {
 		try(var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class)){
-			context.getBean(GamingConsole.class).up();
-			context.getBean(GameRunner.class).run();
+			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 		}
 	}
 

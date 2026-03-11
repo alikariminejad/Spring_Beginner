dda7fa0362821a58d26cb9dd4505d8fd3c13c229
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-11 17:49:42 +0330

    Lazy Initialization example

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/RealWorldSpringContextLauncherApplication.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/RealWorldSpringContextLauncherApplication.java
@@ -5,8 +5,33 @@ import java.util.Arrays;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
 
 
+
+@Component
+class ClassA{
+	
+}
+
+
+@Component
+@Lazy
+class ClassB{
+	
+	private ClassA classA;
+	
+	public ClassB(ClassA classA) {
+		//Logic
+		System.out.println("Some Initalization logic");
+		this.classA = classA;
+	}
+	
+	public void doSomething() {
+		System.out.println("Doing STH!");
+	}
+}
 
 @Configuration
 @ComponentScan
@@ -16,7 +41,9 @@ public class RealWorldSpringContextLauncherApplication {
 		try(var context = new AnnotationConfigApplicationContext(RealWorldSpringContextLauncherApplication.class)){
 			Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
 			
-			System.out.println(context.getBean(BusinessCalculationService.class).findMax());
+			System.out.println("Initialization of context is completed");
+			
+			context.getBean(ClassB.class).doSomething();
 		}
 	}
 

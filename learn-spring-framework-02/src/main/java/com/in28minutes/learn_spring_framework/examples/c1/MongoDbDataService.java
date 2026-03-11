82b809b022e266b4e2dbe07c931b5ef41011a3d8
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-11 21:49:14 +0330

    Service, Repository  Annotations

--- learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/MongoDbDataService.java
+++ learn-spring-framework-02/src/main/java/com/in28minutes/learn_spring_framework/examples/c1/MongoDbDataService.java
@@ -2,8 +2,10 @@ package com.in28minutes.learn_spring_framework.examples.c1;
 
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
 
-@Component
+//@Component
+@Repository
 public class MongoDbDataService implements DataService{
 
 	@Override

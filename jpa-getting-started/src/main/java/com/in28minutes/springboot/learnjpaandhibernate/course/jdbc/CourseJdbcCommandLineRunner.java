31d7c09265d461846379668d64a16fc041223771
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-20 23:25:01 +0330

    Querying Data using Spring JDBC

--- jpa-getting-started/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ jpa-getting-started/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import ch.qos.logback.core.net.SyslogOutputStream;
 import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -15,6 +16,10 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
         repository.insert(new Course(1,"Learn Backend", "Coursera"));
         repository.insert(new Course(2, "Learn AI", "Udemy"));
         repository.insert(new Course(3, "Learn Spring Boot", "DEV.IO"));
+
         repository.delete(1);
+
+        System.out.println(repository.findById(2));
+        System.out.println(repository.findById(3));
     }
 }

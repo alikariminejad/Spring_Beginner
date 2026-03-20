e950407bdea5afa0ad9322fda6e74beb6a832037
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-20 22:45:37 +0330

    Inserting and Deleting Data using Spring JDBC

--- jpa-getting-started/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
+++ jpa-getting-started/src/main/java/com/in28minutes/springboot/learnjpaandhibernate/course/jdbc/CourseJdbcCommandLineRunner.java
@@ -1,5 +1,6 @@
 package com.in28minutes.springboot.learnjpaandhibernate.course.jdbc;
 
+import com.in28minutes.springboot.learnjpaandhibernate.course.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -11,6 +12,9 @@ public class CourseJdbcCommandLineRunner implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        repository.insert();
+        repository.insert(new Course(1,"Learn Backend", "Coursera"));
+        repository.insert(new Course(2, "Learn AI", "Udemy"));
+        repository.insert(new Course(3, "Learn Spring Boot", "DEV.IO"));
+        repository.delete(1);
     }
 }

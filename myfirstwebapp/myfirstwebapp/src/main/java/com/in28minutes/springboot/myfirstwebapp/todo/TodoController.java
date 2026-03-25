4d4e2d86b23bbea0f369e7e6a7eb168b92982eb8
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-25 23:07:32 +0330

    Created a new View for adding todo

--- myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoController.java
@@ -3,6 +3,7 @@ package com.in28minutes.springboot.myfirstwebapp.todo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
 import java.util.List;
@@ -22,5 +23,15 @@ public class TodoController {
         List allTodos = todoService.findByUsername("admin");
         model.addAttribute("todos", allTodos);
         return "todoList";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showAddTodoForm() {
+        return "addTodo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String AddTodo() {
+        return "redirect:todo-list";
     }
 }
---
+++ myfirstwebapp/myfirstwebapp/src/main/resources/META-INF/resources/WEB-INF/JSP/addTodo.jsp
@@ -0,0 +1,18 @@
+<html>
+<title>
+    Add todo
+</title>
+<link href="webjars/bootstrap/5.1.3/css/bootstrap.min.css" rel="stylesheet" >
+
+<body>
+<div class="container">
+<h1>
+    Adding a todo
+</h1>
+    <form method="post">
+        Description: <input type="text" name="description" />
+        <input type="submit" class="btn btn-success" />
+    </form>
+</div>
+</body>
+</html>

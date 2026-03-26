d68b4eebcbd7b4d80181c47dc79ae79f5abda380
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-26 11:21:07 +0330

    Enhanced TodoService to add the todo

--- myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/todo/TodoService.java
@@ -10,13 +10,19 @@ import java.util.List;
 public class TodoService {
 
     private static List<Todo> todos = new ArrayList<>();
+    private static int todosCount = 0;
     static{
-        todos.add(new Todo(1, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1),false));
-        todos.add(new Todo(2, "in28minutes", "Learn AI", LocalDate.now().plusYears(3),false));
-        todos.add(new Todo(3, "in28minutes", "Learn Docker", LocalDate.now().plusYears(2),false));
+        todos.add(new Todo(++todosCount, "in28minutes", "Learn AWS", LocalDate.now().plusYears(1),false));
+        todos.add(new Todo(++todosCount, "in28minutes", "Learn AI", LocalDate.now().plusYears(3),false));
+        todos.add(new Todo(++todosCount, "in28minutes", "Learn Docker", LocalDate.now().plusYears(2),false));
     }
 
     public List<Todo> findByUsername(String username) {
         return todos;
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean done) {
+        Todo todo = new Todo(++todosCount, username, description, targetDate, done);
+        todos.add(todo);
     }
 }

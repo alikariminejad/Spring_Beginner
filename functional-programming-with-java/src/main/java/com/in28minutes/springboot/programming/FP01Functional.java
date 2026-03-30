2d6a7cd090635e96385c7c730d9a6f8575dc1e00
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-03-30 10:06:39 +0330

    Improved Java Functional Program with filter

--- functional-programming-with-java/src/main/java/com/in28minutes/springboot/programming/FP01Functional.java
+++ functional-programming-with-java/src/main/java/com/in28minutes/springboot/programming/FP01Functional.java
@@ -4,14 +4,22 @@ import java.util.List;
 
 public class FP01Functional {
     public static void main(String[] args){
-        printAllNumbersInListFunctional(List.of(1,24,123,141,2525,23,654,778,34));
+        List<Integer> numbers = List.of(1, 24, 123, 141, 2525, 23, 654, 778, 34);
+//        printAllNumbersInListFunctional(numbers);
+        printEvenNumbersInListFunctional(numbers);
     }
 
-    public static void print(int number){
-        System.out.println(number);
-    }
+public static boolean isEven(int number){
+        return number % 2 == 0;
+}
 
     private static void printAllNumbersInListFunctional(List<Integer> numbers) {
-       numbers.stream().forEach(FP01Functional::print); // Method Reference
+       numbers.stream().forEach(System.out::println); // Method Reference
+    }
+
+    private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+        numbers.stream()
+                .filter(FP01Functional::isEven) // filter - Only Allow Even numbers
+                .forEach(System.out::println); // Method Reference
     }
 }
--- functional-programming-with-java/src/main/java/com/in28minutes/springboot/programming/FP01Structured.java
+++ functional-programming-with-java/src/main/java/com/in28minutes/springboot/programming/FP01Structured.java
@@ -1,15 +1,26 @@
 package com.in28minutes.springboot.programming;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FP01Structured {
     public static void main(String[] args){
-        printAllNumbersInListStructured(List.of(1,24,123,141,2525,23,654,778,34));
+        List <Integer> numbers = List.of(1,24,123,141,2525,23,654,778,34);
+//        printAllNumbersInListStructured(numbers);
+        printEvenNumbersInListStructured(numbers);
     }
 
     private static void printAllNumbersInListStructured(List<Integer> numbers) {
         for(int number:numbers){
             System.out.println(number);
+        }
+    }
+
+    private static void printEvenNumbersInListStructured(List<Integer> numbers) {
+        for(int number:numbers){
+            if(number % 2 == 0){
+                System.out.println(number);
+            }
         }
     }
 }

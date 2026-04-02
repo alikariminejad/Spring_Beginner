2b5eced6c36c3d507f37542436272d79c925ca17
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-02 13:01:01 +0330

    Configuring Spring Security to Get H2 console Working

--- myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ myfirstwebapp/myfirstwebapp/src/main/java/com/in28minutes/springboot/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -2,11 +2,14 @@ package com.in28minutes.springboot.myfirstwebapp.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static  org.springframework.security.config.Customizer.withDefaults;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -14,7 +17,7 @@ import java.util.function.Function;
 public class SpringSecurityConfiguration {
 
     @Bean
-    public InMemoryUserDetailsManager createUserDetailsManager(){
+    public InMemoryUserDetailsManager createUserDetailsManager() {
 
         UserDetails userDetails1 = createNewUser("admin", "1234");
         UserDetails userDetails2 = createNewUser("Ali", "1234");
@@ -23,15 +26,38 @@ public class SpringSecurityConfiguration {
 
     private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-        UserDetails userDetails =  User.builder().passwordEncoder(passwordEncoder)
-                                    .username(username)
-                                    .password(password).roles("ADMIN","USER").build();
+        UserDetails userDetails = User.builder().passwordEncoder(passwordEncoder)
+                .username(username)
+                .password(password).roles("ADMIN", "USER").build();
         return userDetails;
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+//        http.formLogin(withDefaults());
+////        http.csrf().disable();
+//        http.csrf(CsrfConfigurer::disable);
+//
+//        http.headers().frameOptions().disable();
+//        return http.build();
+//    }
+
+@Bean
+public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http
+            .authorizeHttpRequests(auth -> auth
+                    .anyRequest().permitAll()
+            )
+            .formLogin(withDefaults())
+            .csrf(csrf -> csrf.disable())
+            .headers(headers -> headers.frameOptions(frame -> frame.disable()));
+    return http.build();
 }
 
+}

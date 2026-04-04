e207e9394a97b4c844360b3d819ad869e46da6e0
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-04 21:40:14 +0330

    Implementing GET Methods for User Resource

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -15,10 +16,15 @@ public class UserDaoService {
     static {
         users.add(new User(1, "Ali", LocalDate.now().minusYears(28)));
         users.add(new User(2, "Aref",  LocalDate.now().minusYears(34)));
-        users.add(new User(3, "Hamed", LocalDate.now().minusYears(28)));
+        users.add(new User(3, "Hamed", LocalDate.now().minusYears(29).plusMonths(6)));
     }
 
     public List<User> findAll(){
         return users;
+    }
+
+    public User findOne(int id){
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 }

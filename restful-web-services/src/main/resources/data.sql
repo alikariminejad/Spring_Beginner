86dd10f2c0387ee5c5b8b32ea3a7ea986a62ffbb
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-09 14:22:52 +0330

    Creating Post Entity with Many to One Relationship with User Entity

--- restful-web-services/src/main/resources/data.sql
+++ restful-web-services/src/main/resources/data.sql
@@ -6,3 +6,15 @@ values(1002,current_date(),'aref');
 
 insert into user_details(id,birth_date, name)
 values(1003,current_date(),'hamed');
+
+insert into post(id,description, user_id)
+values(20001, 'i want to learn docker', 1001);
+
+insert into post(id,description, user_id)
+values(20002, 'i want to learn aws', 1001);
+
+insert into post(id,description, user_id)
+values(20003, 'i want to learn java', 1002);
+
+insert into post(id,description, user_id)
+values(20004, 'i want to learn aws', 1003);

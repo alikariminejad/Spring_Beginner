6fc48b82d28056970eefafbef5688b7ee156dda6
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-16 14:22:19 +0330

    Creating Your First React Component and more

--- full-stack/01-frontend/todo-app/src/App.js
+++ full-stack/01-frontend/todo-app/src/App.js
@@ -1,11 +1,44 @@
 import './App.css';
+import { Component } from 'react';
 
 function App() {
   return (
     <div className="App">
-    My Todo Application  
+      My Todo Application  
+      <FirstComponent></FirstComponent>
+      <SecondComponent></SecondComponent>
+      <ThirdComponent></ThirdComponent>
+      <FourthComponent></FourthComponent>
     </div>
   );
+}
+
+function FirstComponent() {
+  return (
+    <div className='FirstComponent'>Testing First</div>
+  );
+}
+
+function SecondComponent() {
+  return (
+    <div className='SecondComponent'>Second Component</div>
+  );
+}
+
+class ThirdComponent extends Component{
+  render() {
+    return (
+      <div className='ThirdComponent'>Third Component</div>
+    );
+  }
+}
+
+class FourthComponent extends Component{
+  render() {
+    return (
+      <div className='FourthComponent'>Fourth testing</div>
+    );
+  }
 }
 
 export default App;

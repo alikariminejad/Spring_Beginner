42ae1873f06ffddf29e07f06e292ce76f61f6d7f
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-16 15:46:10 +0330

    Following JavaScript Best Practices - Refactoring to Modules

--- full-stack/01-frontend/todo-app/src/App.js
+++ full-stack/01-frontend/todo-app/src/App.js
@@ -1,44 +1,14 @@
 import './App.css';
-import { Component } from 'react';
+import LearningComponent from './components/learning-examples/LearningComponent';
 
 function App() {
   return (
     <div className="App">
-      My Todo Application  
-      <FirstComponent  />
-      <SecondComponent />
-      <ThirdComponent  />
-      <FourthComponent />
+      Using Learning Component
+      <LearningComponent />
     </div>
   );
 }
 
-function FirstComponent() {
-  return (
-    <div className='FirstComponent'>Testing First</div>
-  );
-}
-
-function SecondComponent() {
-  return (
-    <div className='SecondComponent'>Second Component</div>
-  );
-}
-
-class ThirdComponent extends Component{
-  render() {
-    return (
-      <div className='ThirdComponent'>Third Component</div>
-    );
-  }
-}
-
-class FourthComponent extends Component{
-  render() {
-    return (
-      <div className='FourthComponent'>Fourth testing</div>
-    );
-  }
-}
 
 export default App;

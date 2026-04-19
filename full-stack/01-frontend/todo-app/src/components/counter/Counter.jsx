bb10082956f7e229ce240964ed468baa87459fe8
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-19 17:10:36 +0330

    added decrementCounterFunction and a button for this

--- full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -10,12 +10,18 @@ export default function Counter() {
         // console.log('count is:', count)
     }
 
+    function decrementCounterFunction() {
+        setCount(count - 1)
+    }
+
     return (
         <div className="Counter">
-            <span className="count">{ count}</span>
+            <span className="count">{count}</span>
             <div>
-                <button className="counterButton"
-                    onClick={incrementCounterFunction}>+1</button>
+                    <button className="counterButton"
+                        onClick={incrementCounterFunction}>+1</button>
+                    <button className='counterButton'
+                        onClick={decrementCounterFunction}>-1</button>
             </div>
         </div>
     )

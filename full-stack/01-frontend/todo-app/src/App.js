63903f37770746b7eb88e88a3e57329f1eedec11
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-19 16:15:13 +0330

    created counter component

--- full-stack/01-frontend/todo-app/src/App.js
+++ full-stack/01-frontend/todo-app/src/App.js
@@ -1,11 +1,10 @@
 import './App.css';
-import LearningComponent from './components/learning-examples/LearningComponent';
+import Counter from './components/counter/Counter';
 
 function App() {
   return (
     <div className="App">
-      Using Learning Component
-      <LearningComponent />
+      <Counter />
     </div>
   );
 }

2c8bcfcc5cd13d137ee413262716f8dfef70a9c9
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-19 16:35:32 +0330

    styling counter component

--- full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
+++ full-stack/01-frontend/todo-app/src/components/counter/Counter.jsx
@@ -1,4 +1,16 @@
+import './Counter.css'
+
 export default function Counter() {
+
+    const buttonStyle = {
+        fontSize: "16px",
+        backgroundColor: "#00a5ab",
+        width: "100px",
+        margin: "10px",
+        color: "white",
+        padding: "15px",
+        borderRadius: "30px"
+    }
 
     function incrementCounterFunction() {
         console.log('increment clicked')
@@ -6,9 +18,10 @@ export default function Counter() {
 
     return (
         <div className="Counter">
-            <span className="count">+1</span>
+            <span className="count">0</span>
             <div>
-            <button className="counterButton" onClick={incrementCounterFunction}>increment</button>
+                <button className="counterButton"
+                    onClick={incrementCounterFunction}>+1</button>
             </div>
         </div>
     )

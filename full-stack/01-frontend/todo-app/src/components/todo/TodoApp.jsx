d668eeb070be163ef9639f9daa65c70492b19636
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-21 18:17:07 +0330

    Creating React Header, Footer and Logout Components

--- full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
+++ full-stack/01-frontend/todo-app/src/components/todo/TodoApp.jsx
@@ -5,15 +5,22 @@ import './TodoApp.css'
 export default function TodoApp() {
     return (
         <div className="TodoApp">
+            <HeaderComponent />
+            
+
             <BrowserRouter>
                 <Routes>
                     <Route path='/' element={<LoginComponent/>}/>
                     <Route path='/login' element={<LoginComponent />}/>
                     <Route path='/welcome/:username' element={<WelcomeComponent />}/>
-                    <Route path='/todos' element={<ListTodosComponent/>}/>
+                    <Route path='/todos' element={<ListTodosComponent />} />
+                    <Route path='/logout' element={<LogoutComponent/>}/>
                     <Route path='*' element={<ErrorComponent/>}/>
                 </Routes>
             </BrowserRouter>
+
+
+            <FooterComponent/>
         </div>
     )
 }
@@ -129,6 +136,34 @@ function ListTodosComponent() {
                     
                 </tbody>
             </table>
+        </div>
+    )
+}
+
+function HeaderComponent() {
+    return (
+        <div className="HeaderComponent">
+            Header<hr/>
+        </div>
+    )
+}
+
+function FooterComponent() {
+    return (
+        <div className="FooterComponent">
+            <hr/>Footer
+        </div>
+    )
+}
+
+
+function LogoutComponent() {
+    return (
+        <div className="LogoutComponent">
+            <h1>You are logged out!</h1>
+            <div>
+                Thank you for using our website. Come back soon!
+            </div>
         </div>
     )
 }

06c5de6e314a5afee24b990eaed8e18edf8ab24a
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-24 18:06:50 +0330

    Displaying Todos from Spring Boot REST API in React App

--- full-stack/01-frontend/todo-app/src/components/todo/ListTodosComponent.jsx
+++ full-stack/01-frontend/todo-app/src/components/todo/ListTodosComponent.jsx
@@ -1,14 +1,30 @@
+import { useEffect, useState } from "react";
+import { retrieveAllTodosForUsername } from "./api/TodoApiService";
+
 export default function ListTodosComponent() {
 
     const today = new Date();
     const targetDate = new Date(today.getFullYear()+10, today.getMonth(),today.getDay())
 
-    const todos = [
-        { id: 1, description: "Learn Java", done: false, targetDate:targetDate},
-        { id: 2, description: "Learn SpringBoot",done: false, targetDate:targetDate },
-        { id: 3, description: "Learn Docker",done: false, targetDate:targetDate }
-    ]
+    const [todos, setTodos] = useState([])
 
+    // const todos = [
+    //     { id: 1, description: "Learn Java", done: false, targetDate:targetDate},
+    //     { id: 2, description: "Learn SpringBoot",done: false, targetDate:targetDate },
+    //     { id: 3, description: "Learn Docker",done: false, targetDate:targetDate }
+    // ]
+
+    useEffect(() => refreshTodos(), [])
+
+    function refreshTodos() {
+        retrieveAllTodosForUsername('admin')
+            .then(response => {
+                console.log(response)
+                setTodos(response.data)
+            })
+            .catch(error => console.log(error))    
+    }
+    
     return (
         <div className="container">
             <h1>Here are the things you want to do!</h1>
@@ -29,7 +45,8 @@ export default function ListTodosComponent() {
                                     <td>{todo.id}</td>
                                     <td>{todo.description}</td>
                                     <td>{todo.done.toString()}</td>
-                                    <td>{todo.targetDate.toDateString()}</td>
+                                    {/* <td>{todo.targetDate.toDateString()}</td> */}
+                                    <td>{todo.targetDate.toString()}</td>
                                 </tr>
                             )
                         }

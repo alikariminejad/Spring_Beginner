73f68a8e618ddacf89262161650ddf59efd83156
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-25 23:42:27 +0330

    Adding Update Feature to React Frontend

--- full-stack/01-frontend/todo-app/src/components/todo/TodoComponent.jsx
+++ full-stack/01-frontend/todo-app/src/components/todo/TodoComponent.jsx
@@ -1,5 +1,5 @@
-import { useParams } from "react-router-dom"
-import { retrieveTodoApi } from "./api/TodoApiService"
+import { useNavigate, useParams } from "react-router-dom"
+import { retrieveTodoApi, updateTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
 import {Formik, Form, Field, ErrorMessage} from 'formik'
@@ -10,6 +10,7 @@ export default function TodoComponent() {
     const [description, setDescription] = useState('')
     const [targetDate, setTargetDate] = useState('')
     const authContext = useAuth()
+    const navigate = useNavigate()
     const username = authContext.username
     useEffect(
         () => retrieveTodos(),[id]
@@ -26,6 +27,19 @@ export default function TodoComponent() {
 
     function onSubmit(values) {
         console.log(values)
+        const todo = {
+            id: id,
+            username: username,
+            description: values.description,
+            targetDate: values.targetDate,
+            done: false
+
+        }
+        updateTodoApi(username, id, todo)
+            .then(response => {
+            navigate('/todos')
+            })
+        .catch(error => console.log(error))
     }
     
     function validate(values) {

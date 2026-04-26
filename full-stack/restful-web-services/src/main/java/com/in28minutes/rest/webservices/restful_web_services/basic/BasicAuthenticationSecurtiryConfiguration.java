ea23ae11a6ff46159928f65f2883673665e56e39
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-04-26 12:50:55 +0330

    Configuring Spring Security to allow all Options Requests

--- full-stack/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/basic/BasicAuthenticationSecurtiryConfiguration.java
+++ full-stack/restful-web-services/src/main/java/com/in28minutes/rest/webservices/restful_web_services/basic/BasicAuthenticationSecurtiryConfiguration.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restful_web_services.basic;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -13,7 +14,9 @@ public class BasicAuthenticationSecurtiryConfiguration {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return
-            http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+            http.authorizeHttpRequests(auth -> auth
+                            .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                            .anyRequest().authenticated())
                     .httpBasic(Customizer.withDefaults())
                     .sessionManagement(
                 session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))

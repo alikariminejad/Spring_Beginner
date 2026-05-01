75a2aa2410f17ed7a2670207d5ce93dbdb15b24a
Ali Kariminejad <kariminejad.ali.ak@gmail.com>
2026-05-01 19:17:16 +0330

    Exploring Spring Security - Cross Site Request Forgery - CSRF

--- learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/resources/HelloWorldResource.java
+++ learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/resources/HelloWorldResource.java
@@ -1,4 +1,4 @@
-package com.in28minutes.learn_spring_security;
+package com.in28minutes.learn_spring_security.resources;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ learn-spring-security/src/main/java/com/in28minutes/learn_spring_security/resources/TodoResource.java
@@ -0,0 +1,34 @@
+package com.in28minutes.learn_spring_security.resources;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class TodoResource {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    private static final List<Todo> TODO_LIST =
+            List.of(new Todo("admin", "learn AWS"),
+                    new Todo("admin", "Learn MicroService"));
+
+    @GetMapping("/todos")
+    public List<Todo> retrieveAllTodos(){
+        return TODO_LIST;
+    }
+
+    @GetMapping("/users/{username}/todos")
+    public Todo retrieveTodosForSpecificUser(@PathVariable String username){
+        return TODO_LIST.get(0);
+    }
+
+    @PostMapping("/users/{username}/todos")
+    public void createTodoForSpecificUser(@PathVariable String username, @RequestBody Todo todo){
+        logger.info("Create {} for {}", todo, username);
+    }
+}
+
+record Todo (String username, String description){}
